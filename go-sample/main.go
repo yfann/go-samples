01f92a302624359bbca624a94eef3b73804a1d9e
fan <fan.yu@philips.com>
2021-07-22 17:27:48 +0800

    update

--- go-sample/main.go
+++ go-sample/main.go
@@ -7,21 +7,16 @@ import (
 	"strings"
 )
 
-func main()  {
+func main() {
 	//sample.CreateFolder()
 
-
-
 	//sample.TestObj()
-
 
 	//sample.TestTime()
 
 	//sample.TestReg()
 
-
 	//testMap()
-
 
 	//sample.CreateFile()
 
@@ -31,15 +26,15 @@ func main()  {
 	//delVault()
 	//sample.TestStr()
 	//fmt.Println(vault.ParseToken(`
-	//	Unseal Key 1: gF7p3O8ylB1eCsMbGJ+naGhwI9N4bW9hNQ6+CxeRmETl
-	//	Unseal Key 2: 3oImWYgPw3DdUem2x5cQylTygpFt6PY8lr+lx48QYtqa
-	//	Unseal Key 3: ndst86syGIZP6kUZDPRe4zq4ut93NIwyiSBHu+jaOpIV
-	//	Unseal Key 4: dx4nGPlUXyHEkbyIi+Tr8d22kM4asBSAUl3DGv/4Tv1A
-	//	Unseal Key 5: KUCaOGLqEuIwyd57XSeXZPC+sSByORWVIDHXHGaMUMn/
+	//	Unseal Key 1: .....
+	//	Unseal Key 2: .....
+	//	Unseal Key 3: .....
+	//	Unseal Key 4: .....
+	//	Unseal Key 5: .....
 	//
-	//	Initial Root Token: s.EWrPgKkNHm2OAa4wJqVpTAxw
-    //`))
-    //sample.TestHash()
+	//	Initial Root Token:.....
+	//`))
+	//sample.TestHash()
 	//vault.TestK8sAuth()
 	//var x interface{} = "abc"
 	//fmt.Println(fmt.Sprintf("%v", x))
@@ -48,34 +43,33 @@ func main()  {
 	vault.AddTransitKey("test")
 }
 
-func testVault(){
-	c:=vault.GetVaultClient()
+func testVault() {
+	c := vault.GetVaultClient()
 
-	sec,err:=c.Logical().Write("/kv/test/vault/",map[string]interface{}{
-		"name":"yfann",
-		"age":"100",
-		"pwd":"asdf9s0fd90sf9s0f",
+	sec, err := c.Logical().Write("/kv/test/vault/", map[string]interface{}{
+		"name": "yfann",
+		"age":  "100",
+		"pwd":  "asdf9s0fd90sf9s0f",
 	})
 
 	fmt.Println(sec)
 	fmt.Println(err)
 }
 
-func delVault(){
-	c:=vault.GetVaultClient()
+func delVault() {
+	c := vault.GetVaultClient()
 
 	c.Logical().Delete("/kv/test/vault")
 }
 
-
-func testStr(){
+func testStr() {
 	//fmt.Println(strings.Replace("$sdfg sdf as $sdfg","$sdfg","@@@@",-1))
-	a:="namespace:<$project>"
-	fmt.Println(strings.Replace(a,"<$project>","123",1))
+	a := "namespace:<$project>"
+	fmt.Println(strings.Replace(a, "<$project>", "123", 1))
 	fmt.Println(a)
 
-	b:="asdfasdf| 12332"
-	fmt.Println(strings.Split(b,"|"))
+	b := "asdfasdf| 12332"
+	fmt.Println(strings.Split(b, "|"))
 
 	fmt.Println(strings.HasPrefix("my string", "my  s"))
 	fmt.Println(strings.HasPrefix("my string", "xxx"))
@@ -87,24 +81,22 @@ func testStr(){
 	fmt.Println(strconv.QuoteToASCII("Hello, 世界"))
 }
 
-func testMap(){
+func testMap() {
 	var a map[string]string
-	a=make(map[string]string)
-	a["a"]="a"
+	a = make(map[string]string)
+	a["a"] = "a"
 	fmt.Println(a)
 }
 
-func testSlice(){
+func testSlice() {
 	var s []string
 	var c []string
 
-	s=append(s,"test")
-	fmt.Println(c==nil)
+	s = append(s, "test")
+	fmt.Println(c == nil)
 
-	s=append(s,c...)
+	s = append(s, c...)
 	fmt.Println(s)
 
-
-	fmt.Println(strings.Join([]string{"sdfsdf","sdfsdf","apple"}, ","))
+	fmt.Println(strings.Join([]string{"sdfsdf", "sdfsdf", "apple"}, ","))
 }
-
